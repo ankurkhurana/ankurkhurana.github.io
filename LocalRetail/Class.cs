445c2cec5d2e4338c740b20fdc92b004a6933694
AnkurKhurana <ankur_khurana@hotmail.com>
2021-01-24 16:56:26 -0500

    First Check in
    My first commit

---
+++ LocalRetail/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LocalRetail
+{
+    public class Student
+    {
+        public string Name { get; set; } = "Search....";
+
+        public DateTime Dob { get; set; }
+    }
+}
---
+++ LocalRetail/ExampleModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LocalRetail
+{
+    public class ExampleModel
+    {
+        public string Name { get; set; } = "hello";
+    }
+}
